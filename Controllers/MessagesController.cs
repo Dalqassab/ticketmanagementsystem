b85b1008f85078f612a6ec732cafbf4a9c4f4ea9
Dalqassa <Dalqassa@asu.edu>
2023-11-22 04:21:12 +0300

    Changing font on header and menu items

--- Controllers/MessagesController.cs
+++ Controllers/MessagesController.cs
@@ -1,15 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Security.Claims;
-using System.Threading.Tasks;
+﻿using System.Security.Claims;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using TicketManagementSystem.Data;
 using TicketManagementSystem.Models;
-
 namespace TicketManagementSystem.Controllers
 {
     [Authorize]
